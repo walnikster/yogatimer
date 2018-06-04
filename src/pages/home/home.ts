e32fcc7cb78d95330b486877cdb76d8619ec1a6c
niki <niki@theliferocks.com>
2018-06-04 20:49:35 +0200

    add timer taking

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { Component } from "@angular/core"
+import { NavController } from "ionic-angular"
+import { Observable } from "rxjs"
+import { takeWhile, tap } from "rxjs/operators"
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  currentval: number
+  started: boolean
+  stopped: boolean
+  timer = Observable.timer(0, 5)
 
   constructor(public navCtrl: NavController) {
-
+    this.currentval = 0.0
   }
 
+  onStart() {
+    this.started = true
+    this.stopped = false
+    this.timer
+      .pipe(takeWhile(_ => !this.stopped), tap(i => (this.currentval += 0.005)))
+      .subscribe()
+  }
+
+  onStop() {
+    this.stopped = true
+  }
+
+  onResume() {
+    this.onStart()
+  }
+
+  onSave() {
+    this.started = false
+    this.stopped = true
+    this.currentval = 0.0
+  }
 }
